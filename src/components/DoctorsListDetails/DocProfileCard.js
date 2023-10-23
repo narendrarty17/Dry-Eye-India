583410d934c070ba8685859204e1138da5de125f
narendrarty17 <narendrarty17@gmail.com>
2023-10-23 09:24:19 -0700

    css design changes

--- src/components/DoctorsListDetails/DocProfileCard.js
+++ src/components/DoctorsListDetails/DocProfileCard.js
@@ -11,66 +11,70 @@ const DocProfileCard = () => {
     })
     return (
         <div className={styles.container}>
-            <div className={styles.header}>
-                <img className={styles.drPic} alt="" src="/pro-pic@2x.png" />
-                <div className={styles.drDetail}>
-                    <div>
-                        <div className={styles.drName}>{selectedDoc.docName}</div>
-                        <div className={styles.drSpecialityAndHospital}>
-                            {selectedDoc.specialization}
-                            <br />
-                            {selectedDoc.clinicAddr}
+            <div className={styles.part1}>
+                <div className={styles.header}>
+                    <img className={styles.drPic} alt="" src="/pro-pic@2x.png" />
+                    <div className={styles.drDetail}>
+                        <div>
+                            <div className={styles.drName}>{selectedDoc.docName}</div>
+                            <div className={styles.drSpecialityAndHospital}>
+                                {selectedDoc.specialization}
+                                <br />
+                                {selectedDoc.clinicAddr}
+                            </div>
+                        </div>
+                        <div className={styles.drContactMediaIcons}>
+                            <img className={styles.contactIcon} alt="" src="/group-14.svg" />
+                            <img className={styles.contactIcon} alt="" src="/group-13.svg" />
+                            <img className={styles.contactIcon} alt="" src="/group-12.svg" />
                         </div>
                     </div>
-                    <div className={styles.drContactMediaIcons}>
-                        <img className={styles.contactIcon} alt="" src="/group-14.svg" />
-                        <img className={styles.contactIcon} alt="" src="/group-13.svg" />
-                        <img className={styles.contactIcon} alt="" src="/group-12.svg" />
+                    <div className={styles.placeHolder} />
+                </div>
+                <div className={styles.about}>
+                    <b className={styles.aboutHeading}>About Doctor</b>
+                    <div className={styles.aboutContent}>
+                        {selectedDoc.about}
                     </div>
                 </div>
-                <div className={styles.placeHolder} />
             </div>
-            <div className={styles.about}>
-                <b className={styles.aboutHeading}>About Doctor</b>
-                <div className={styles.aboutContent}>
-                    {selectedDoc.about}
-                </div>
-            </div>
-            <div className={styles.upcomingSchedules}>
-                <b className={styles.upcomingSchedulesHeading}>Upcoming Schedules</b>
-                <div className={styles.schedules}>
-                    <div className={styles.scheduleDetails}>
-                        <div className={styles.scheduleDate} >
-                            <div className={styles.month}>Jan</div>
-                            <div className={styles.date}>12</div>
+            <div className={styles.part2}>
+                <div className={styles.upcomingSchedules}>
+                    <b className={styles.upcomingSchedulesHeading}>Upcoming Schedules</b>
+                    <div className={styles.schedules}>
+                        <div className={styles.scheduleDetails}>
+                            <div className={styles.scheduleDate} >
+                                <div className={styles.month}>Jan</div>
+                                <div className={styles.date}>12</div>
+                            </div>
+                            <div className={styles.scheduleNameDayTime}>
+                                <b className={styles.scheduleName}>Consultation</b>
+                                <div className={styles.scheduleDayTime}>Sunday . 9am - 11am</div>
+                            </div>
+                            <div className={styles.placeHolder} />
                         </div>
-                        <div className={styles.scheduleNameDayTime}>
-                            <b className={styles.scheduleName}>Consultation</b>
-                            <div className={styles.scheduleDayTime}>Sunday . 9am - 11am</div>
+                        <div className={styles.scheduleDetails}>
+                            <div className={styles.scheduleDate} >
+                                <div className={styles.month}>Jan</div>
+                                <div className={styles.date}>12</div>
+                            </div>
+                            <div className={styles.scheduleNameDayTime}>
+                                <b className={styles.scheduleName}>Consultation</b>
+                                <div className={styles.scheduleDayTime}>Sunday . 9am - 11am</div>
+                            </div>
+                            <div className={styles.placeHolder} />
                         </div>
-                        <div className={styles.placeHolder} />
-                    </div>
-                    <div className={styles.scheduleDetails}>
-                        <div className={styles.scheduleDate} >
-                            <div className={styles.month}>Jan</div>
-                            <div className={styles.date}>12</div>
+                        <div className={styles.scheduleDetails}>
+                            <div className={styles.scheduleDate} >
+                                <div className={styles.month}>Jan</div>
+                                <div className={styles.date}>12</div>
+                            </div>
+                            <div className={styles.scheduleNameDayTime}>
+                                <b className={styles.scheduleName}>Consultation</b>
+                                <div className={styles.scheduleDayTime}>Sunday . 9am - 11am</div>
+                            </div>
+                            <div className={styles.placeHolder} />
                         </div>
-                        <div className={styles.scheduleNameDayTime}>
-                            <b className={styles.scheduleName}>Consultation</b>
-                            <div className={styles.scheduleDayTime}>Sunday . 9am - 11am</div>
-                        </div>
-                        <div className={styles.placeHolder} />
-                    </div>
-                    <div className={styles.scheduleDetails}>
-                        <div className={styles.scheduleDate} >
-                            <div className={styles.month}>Jan</div>
-                            <div className={styles.date}>12</div>
-                        </div>
-                        <div className={styles.scheduleNameDayTime}>
-                            <b className={styles.scheduleName}>Consultation</b>
-                            <div className={styles.scheduleDayTime}>Sunday . 9am - 11am</div>
-                        </div>
-                        <div className={styles.placeHolder} />
                     </div>
                 </div>
             </div>
