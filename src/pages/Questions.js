d4850af8170d58a3d7f812b7993fc3918b203e53
narendrarty17 <narendrarty17@gmail.com>
2023-10-25 13:05:19 -0700

    updated ui of questions pg

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,14 +1,62 @@
+import styles from './Questions.module.css';
 import Header from "../components/CommonComp/Header";
 import Footer from "../components/CommonComp/Footer";
-import QuestionCard from '../components/Diagnosis/QuestionCard';
-
-import styles from './Questions.module.css';
+import { useNavigate } from 'react-router-dom';
 
 const Questions = () => {
+    const navigate = useNavigate();
     return (
-        <div className={styles.questionsCardContainer}>
+        <div>
             <Header />
-            <QuestionCard />
+            <div className={styles.container}>
+                <div className={styles.question}>
+                    <div className={styles.questionLabel}>Question 1/9</div>
+                    <div className={styles.questionMain}>
+                        <div className={styles.questionTxt}>
+                            For Fiscal year 2021, what was your total GhG Carbon Emission for all scopes?
+                        </div>
+                        <div className={styles.checkboxesContainer}>
+                            <div className={styles.option}>
+                                <input className={styles.checkbox} type="checkbox" name="option1" />
+                                <label for="option1">
+                                    &nbsp;&nbsp;Scope 1 - Determine environmental impact levels.
+                                </label>
+                            </div>
+                            <div className={styles.option}>
+                                <input className={styles.checkbox} type="checkbox" name="option2" />
+                                <label for="option2">
+                                    &nbsp;&nbsp;Scope 2 - Reduce carbon footprints.
+                                </label>
+                            </div>
+                            <div className={styles.option}>
+                                <input className={styles.checkbox} type="checkbox" name="option3" />
+                                <label for="option3">
+                                    &nbsp;&nbsp;Scope 3 - Enhance environmental impacts on a larger scale.
+                                </label>
+                            </div>
+                            <div className={styles.option}>
+                                <input className={styles.checkbox} type="checkbox" name="option4" />
+                                <label for="option4">
+                                    &nbsp;&nbsp;I do not know the answer to the question
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.btns}>
+                        <button
+                            className={styles.previous}
+                        >
+                            Previous
+                        </button>
+                        <button
+                            className={styles.next}
+                            onClick={() => navigate('/results')}
+                        >
+                            Next
+                        </button>
+                    </div>
+                </div>
+            </div>
             <Footer />
         </div>
     );
