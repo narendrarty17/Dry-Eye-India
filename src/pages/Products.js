74a5494d0e03f3ef186e15cce50a9a88e1b72489
narendrarty17 <narendrarty17@gmail.com>
2023-10-29 10:31:24 -0700

    added promotion img with slider

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -20,13 +20,33 @@ const Products = () => {
                         />
                     </div>
                     <img
-                        className={styles.loveIcon}
+                        className={styles.topbarSideIcon}
                         src="/images/Products/topbar/love.svg"
                     />
                     <img
-                        className={styles.notificationIcon}
+                        className={styles.topbarSideIcon}
                         src="/images/Products/topbar/notification.svg"
                     />
+                </div>
+                <div className={styles.promotionContainer}>
+                    <div className={styles.promotionImg}>
+                        <h2 className={styles.promotionImgTxt}>
+                            Super Flash Sale<br />
+                            50% off
+                        </h2>
+                        <h4 className={styles.promotionCountDown}>
+                            <div className={styles.countDownBox}>08</div>
+                            <div className={styles.countDownBox}>34</div>
+                            <div className={styles.countDownBox}>52</div>
+                        </h4>
+                    </div>
+                    <ul className={styles.sliderIcons}>
+                        <li className={styles.sliderIcon} />
+                        <li className={styles.sliderIcon} />
+                        <li className={styles.sliderIconSelected} />
+                        <li className={styles.sliderIcon} />
+                        <li className={styles.sliderIcon} />
+                    </ul>
                 </div>
             </div>
         </div>
