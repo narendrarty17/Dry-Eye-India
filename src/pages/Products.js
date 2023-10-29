c0cca1376988a5421c3e150156cf7a5a49e74db0
narendrarty17 <narendrarty17@gmail.com>
2023-10-29 11:50:04 -0700

    add category section in products page

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -48,10 +48,48 @@ const Products = () => {
                         <li className={styles.sliderIcon} />
                     </ul>
                 </div>
-                <div className={styles.category}>
+                <div className={styles.categoryContainer}>
                     <div className={styles.categoryHeader}>
                         <h4 className={styles.categoryLeftHeading}>Category</h4>
                         <h4 className={styles.categoryRightHeading}>More Category</h4>
+                    </div>
+                    <div className={styles.categories}>
+                        <div className={styles.category}>
+                            <div className={styles.categoryIconContainer}>
+                                <img
+                                    className={styles.categoryIcon}
+                                    src="images/Products/categoryIcons/shirt.svg"
+                                />
+                            </div>
+                            <div className={styles.categoryName}>Man Shirt</div>
+                        </div>
+                        <div className={styles.category}>
+                            <div className={styles.categoryIconContainer}>
+                                <img
+                                    className={styles.categoryIcon}
+                                    src="images/Products/categoryIcons/dress.svg"
+                                />
+                            </div>
+                            <div className={styles.categoryName}>Dress</div>
+                        </div>
+                        <div className={styles.category}>
+                            <div className={styles.categoryIconContainer}>
+                                <img
+                                    className={styles.categoryIcon}
+                                    src="images/Products/categoryIcons/manBag.svg"
+                                />
+                            </div>
+                            <div className={styles.categoryName}>Man Bag</div>
+                        </div>
+                        <div className={styles.category}>
+                            <div className={styles.categoryIconContainer}>
+                                <img
+                                    className={styles.categoryIcon}
+                                    src="images/Products/categoryIcons/womanBag.svg"
+                                />
+                            </div>
+                            <div className={styles.categoryName}>Bag</div>
+                        </div>
                     </div>
                 </div>
             </div>
