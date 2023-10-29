6a6c6b3450cfacfc2b416264f43ea41c5947a232
narendrarty17 <narendrarty17@gmail.com>
2023-10-29 09:20:38 -0700

    Update Products.js

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,28 +1,33 @@
 import styles from './Products.module.css';
+import Header from "../components/CommonComp/Header";
+import Footer from "../components/CommonComp/Footer";
 
 const Products = () => {
     return (
-        <div className={styles.container}>
-            <div className={styles.topbar}>
-                <div className={styles.searchbar}>
+        <div>
+            <Header />
+            <div className={styles.container}>
+                <div className={styles.topbar}>
+                    <div className={styles.searchbar}>
+                        <img
+                            className={styles.searchIcon}
+                            src="images/Products/topbar/search.svg"
+                        />
+                        <input
+                            className={styles.searchinput}
+                            type="text"
+                            placeholder="Search Product"
+                        />
+                    </div>
                     <img
-                        className={styles.searchIcon}
-                        src="images/Products/topbar/search.svg"
+                        className={styles.loveIcon}
+                        src="/images/Products/topbar/love.svg"
                     />
-                    <input
-                        className={styles.searchinput}
-                        type="text"
-                        placeholder="Search Product"
+                    <img
+                        className={styles.notificationIcon}
+                        src="/images/Products/topbar/notification.svg"
                     />
                 </div>
-                <img
-                    className={styles.loveIcon}
-                    src="/images/Products/topbar/love.svg"
-                />
-                <img
-                    className={styles.notificationIcon}
-                    src="/images/Products/topbar/notification.svg"
-                />
             </div>
         </div>
     );
